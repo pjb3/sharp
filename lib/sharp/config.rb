2b17ebda2fdd9ab9416fb1b89802cdda81bbf963
Paul Barry <mail@paulbarry.com>
2013-03-11 14:19:08 +0100

    Remove debugging statements

--- lib/sharp/config.rb
+++ lib/sharp/config.rb
@@ -2,9 +2,7 @@ module Sharp
   class Config
     def initialize(env, files)
       files.each do |file|
-        puts file.inspect
         attr = File.basename(file, '.yml').to_sym
-        puts attr.inspect
         (class << self; self; end).send(:attr_accessor, attr)
         send("#{attr}=", YAML.load_file(file).symbolize_keys[env].symbolize_keys)
       end
